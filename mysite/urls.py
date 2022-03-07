96027fdc3ae290cb4fd1bf132763097a8b640b12
Roger Pineda <roger.pinedapaz@gmail.com>
2022-03-07 12:17:07 -0500

    Tarea 9

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webpag.views import pagina
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pagina/', pagina),
 ]
--- webpag/views.py
+++ webpag/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def pagina(request):
+    return render(request, "pagina.html")
